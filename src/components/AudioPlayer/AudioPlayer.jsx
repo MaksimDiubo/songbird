d24da79e064d382c1189aa0f89ae73858bc2dd98
Maksim Diubo <diubomaksim@gmail.com>
2020-08-05 10:33:30 +0300

    feat: add default audio player

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -4,7 +4,13 @@ import './AudioPlayer.scss';
 
 const AudioPlayer = () => {
   return (
-    <div className= 'audioPlayer' />
+    <audio
+      className='audioPlayer'
+      controls
+      src='https://www.xeno-canto.org/sounds/uploaded/BLMSIUFTFU/XC512582-190604_1087_Grus_tok.mp3'
+    >
+      <track kind="captions" />
+    </audio>
   );
 }
 
